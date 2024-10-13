b8c3f91f49c64836685dbd753c2597333f135795
Spring-Rebirth <cjunwei6249@gmail.com>
2024-10-13 19:18:45 +0800

    解决tailwind文件报错

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-const { content } = require('./tailwind.config');
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
