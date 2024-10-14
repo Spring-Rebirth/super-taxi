90f48d02f6539f2b1b457252e35b10684bbccd3d
Spring-Rebirth <cjunwei6249@gmail.com>
2024-10-15 02:03:12 +0800

    2

--- app/(api)/user+api.js
+++ app/(api)/user+api.js
@@ -2,7 +2,36 @@ import { neon } from '@neondatabase/serverless';
 
 const sql = neon(`${process.env.DATABASE_URL}`);
 
-const posts = await sql('SELECT * FROM posts');
+export async function POST(request) {
+    try {
+        const { name, email, clerkId } = await request.json();
+
+        if (!name || !email || !clerkId) {
+            return Response.json(
+                { error: 'Missing required fields' },
+                { status: 400 }
+            );
+        }
+
+        const response = await sql`
+            INSERT INTO users (
+                name,
+                email,
+                clerk_id
+            )
+            VALUES (
+                ${name},
+                ${email},
+                ${clerkId}
+            )
+        `;
+
+        return new Response(JSON.stringify({ data: response }), { status: 201 });
+    } catch (error) {
+        console.log(error);
+        return Response.json({ error: error }, { status: 500 });
+    }
+}
 
 // See https://neon.tech/docs/serverless/serverless-driver
 // for more information
