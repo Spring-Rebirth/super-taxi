e4cb169b35225e9cc599f2031ca1a42d976d475c
Spring-Rebirth <cjunwei6249@gmail.com>
2024-10-16 00:52:33 +0800

    一切正常

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,6 +1,11 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
 import { ClerkProvider, ClerkLoaded, SignedIn, SignedOut } from '@clerk/clerk-expo';
 import { tokenCache } from '../lib/clerk/auth'
+import { LogBox } from "react-native";
+import { useEffect } from "react";
+
+// 防止自动隐藏启动屏幕
+SplashScreen.preventAutoHideAsync();
 
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
 
@@ -10,7 +15,14 @@ if (!publishableKey) {
   );
 }
 
+// 忽略特定的日志
+LogBox.ignoreLogs(["Clerk:"]);
+
 export default function RootLayout() {
+  // 直接在组件挂载后隐藏启动屏幕
+  useEffect(() => {
+    SplashScreen.hideAsync();
+  }, []);
   return (
     <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
       <ClerkLoaded>
