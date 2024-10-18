6afaf0172190edb03dd9942134331082711dc2bc
Spring-Rebirth <cjunwei6249@gmail.com>
2024-10-18 19:41:49 +0800

    配置'zustand' store

---
+++ store/index.js
@@ -0,0 +1,24 @@
+import { create } from "zustand";
+
+export const useLocationStore = create((set) => ({
+    userAddress: null,
+    userLongitude: null,
+    userLatitude: null,
+    destinationLongitude: null,
+    destinationLatitude: null,
+    destinationAddress: null,
+    setUserLocation: ({ latitude, longitude, address }) => {
+        set(() => ({
+            userLatitude: latitude,
+            userLongitude: longitude,
+            userAddress: address
+        }))
+    },
+    setDestinationLocation: ({ latitude, longitude, address }) => {
+        set(() => ({
+            destinationLatitude: latitude,
+            destinationLongitude: longitude,
+            destinationAddress: address
+        }))
+    }
+}))
