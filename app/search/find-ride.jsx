5ac1f4b6807695942c62da8c224f5719051755f2
Spring-Rebirth <cjunwei6249@gmail.com>
2024-10-21 06:09:33 +0800

    暂存

--- app/search/find-ride.jsx
+++ app/search/find-ride.jsx
@@ -1,7 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useLocationStore } from '../../store'
 
 export default function FindRide() {
+    const { userAddress, destinationAddress, setUserLocation, setDestinationLocation } = useLocationStore();
+
     return (
         <View>
             <Text>FindRide</Text>
