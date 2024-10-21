ced70e0cac66cbf650b2a04cec83c17c3d259c3a
Spring-Rebirth <cjunwei6249@gmail.com>
2024-10-21 19:29:30 +0800

    传递props在find-ride页面

--- app/search/find-ride.jsx
+++ app/search/find-ride.jsx
@@ -7,6 +7,7 @@ import targetIcon from '../../assets/icons/target.png'
 import mapIcon from '../../assets/icons/map.png'
 import CustomButton from '../../components/CustomButton'
 import axios from 'axios'
+import { router } from 'expo-router'
 
 
 export default function FindRide() {
@@ -24,6 +25,27 @@ export default function FindRide() {
         }
     };
 
+    const handleResultPress = (item) => {
+        // 在这里处理用户点击某个搜索结果的逻辑
+        console.log('Selected location:', JSON.stringify(item, null, 2));
+        const { address, lat, lon } = item;
+        const formattedAddress = [
+            address.house_number && address.road ? `${address.house_number} ${address.road}` : address.road,  // 检查 house_number 和 road
+            address.town,
+            address.state && address.postcode ? `${address.state} ${address.postcode}` : address.state || address.postcode,  // 检查 state 和 postcode
+            address.country
+        ].filter(Boolean).join(', ');
+
+        setDestinationLocation({
+            latitude: parseFloat(lat),  // 确保转换为浮点数
+            longitude: parseFloat(lon), // 确保转换为浮点数
+            address: formattedAddress   // 一个字符串
+        });
+
+        router.push('/search/find-ride');
+
+        setSearchResults([]); // 选择后隐藏搜索结果
+    };
 
     return (
         <>
@@ -33,11 +55,13 @@ export default function FindRide() {
                 </Text>
                 <View className='items-center'>
                     <OSMTextInput
-                        onSearch={searchLocation}
                         containerStyle={'bg-neutral-100'}
                         icon={targetIcon}
                         textInputStyle={{ backgroundColor: 'transparent' }}
                         value={userAddress}
+                        onSearch={searchLocation}
+                        searchResults={searchResults}
+                        onSelectResult={handleResultPress}
                     />
                 </View>
             </View>
@@ -52,6 +76,9 @@ export default function FindRide() {
                         icon={mapIcon}
                         textInputStyle={{ backgroundColor: 'transparent' }}
                         value={destinationAddress}
+                        onSearch={searchLocation}
+                        searchResults={searchResults}
+                        onSelectResult={handleResultPress}
                     />
                 </View>
             </View>
