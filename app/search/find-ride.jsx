c8d7876d18c1997263cd479599df532204bc86d3
Spring-Rebirth <cjunwei6249@gmail.com>
2024-10-21 17:59:02 +0800

    正确传递OSM props

--- app/search/find-ride.jsx
+++ app/search/find-ride.jsx
@@ -1,16 +1,28 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { useLocationStore } from '../../store'
 import OSMTextInput from '../../components/OSMTextInput'
 import searchIcon from '../../assets/icons/search.png'
 import targetIcon from '../../assets/icons/target.png'
 import mapIcon from '../../assets/icons/map.png'
 import CustomButton from '../../components/CustomButton'
+import axios from 'axios'
+
 
 export default function FindRide() {
     const { userAddress, destinationAddress, setUserLocation, setDestinationLocation } = useLocationStore();
+    const [searchResults, setSearchResults] = useState([]);
 
-
+    const searchLocation = async (query) => {
+        if (query.length > 2) {
+            const response = await axios.get(
+                `https://nominatim.openstreetmap.org/search?q=${query}&format=json&addressdetails=1&limit=5`
+            );
+            setSearchResults(response.data); // 设置搜索结果
+        } else {
+            setSearchResults([]); // 如果输入少于3个字符，清空结果
+        }
+    };
 
 
     return (
@@ -21,6 +33,7 @@ export default function FindRide() {
                 </Text>
                 <View className='items-center'>
                     <OSMTextInput
+                        onSearch={searchLocation}
                         containerStyle={'bg-neutral-100'}
                         icon={targetIcon}
                         textInputStyle={{ backgroundColor: 'transparent' }}
