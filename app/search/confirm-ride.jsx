b6df1b9e3b4a30e93ba9f5278b7b88d613f21837
Spring-Rebirth <cjunwei6249@gmail.com>
2024-10-21 23:44:54 +0800

    设置司机卡片按钮位置

--- app/search/confirm-ride.jsx
+++ app/search/confirm-ride.jsx
@@ -2,12 +2,20 @@ import { View, Text, FlatList } from 'react-native'
 import React from 'react'
 import { driverMock } from '../../constants/MockDrivers'
 import DriverCard from '../../components/DriverCard'
+import CustomButton from '../../components/CustomButton'
 
 export default function ConfirmRide() {
     return (
         <View>
             <FlatList
                 data={driverMock}
+                ListFooterComponent={() => (
+                    <View className='mt-3'>
+                        <CustomButton
+                            title={'Select Ride'}
+                        />
+                    </View>
+                )}
                 renderItem={({ item }) => (
                     <DriverCard
                         item={item}
