c3f398983df4fb9206109cd534a3ea08519f2120
Spring-Rebirth <cjunwei6249@gmail.com>
2024-10-22 21:21:48 +0800

    修改默认内容4

--- app/(api)/stripe/create+api.js
+++ app/(api)/stripe/create+api.js
@@ -1,3 +1,4 @@
+import { create } from 'react-test-renderer';
 import { Stripe } from 'stripe'
 
 const stripe = new Stripe(process.env.EXPO_SECRET_STRIPE_API_KEY);
@@ -28,6 +29,31 @@ export async function POST(request) {
 
         customer = newCustomer;
     }
+
+    const ephemeralKey = await stripe.ephemeralKeys.create(
+        { customer: customer.id },
+        { apiVersion: '2024-09-30.acacia' }
+    );
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: parseInt(amount) * 100,
+        currency: 'usd',
+        customer: customer.id,
+        // In the latest version of the API, specifying the `automatic_payment_methods` parameter
+        // is optional because Stripe enables its functionality by default.
+        automatic_payment_methods: {
+            enabled: true,
+            allow_redirects: 'never'
+        },
+    });
+
+    return new Response(
+        JSON.stringify({
+            paymentIntent: paymentIntent.client_secret,
+            ephemeralKey: ephemeralKey.secret,
+            customer: customer.id,
+            publishableKey: 'pk_test_51QCQ22FtcuUTeLbKDvnvy8gGAuaeGDOTqjCgMjp8FGXvzdtRNHroyDrG6oZwPackuIHdiGSg0FTXwFcDweegTUey00VmF3aOgV'
+        })
+    );
 }
 
 // app.post('/payment-sheet', async (req, res) => {
