7bc24bde0880cc95349d0b94deb470d4290f432b
Spring-Rebirth <cjunwei6249@gmail.com>
2024-10-22 21:24:58 +0800

    删除默认无用代码

--- app/(api)/stripe/create+api.js
+++ app/(api)/stripe/create+api.js
@@ -55,25 +55,3 @@ export async function POST(request) {
     );
 }
 
-// app.post('/payment-sheet', async (req, res) => {
-//     // Use an existing Customer ID if this is a returning customer.
-//     const customer = await stripe.customers.create();
-//     const ephemeralKey = await stripe.ephemeralKeys.create(
-//         { customer: customer.id },
-//         { apiVersion: '2024-09-30.acacia' }
-//     );
-//     const setupIntent = await stripe.setupIntents.create({
-//         customer: customer.id,
-//         // In the latest version of the API, specifying the `automatic_payment_methods` parameter
-//         // is optional because Stripe enables its functionality by default.
-//         automatic_payment_methods: {
-//             enabled: true,
-//         },
-//     });
-//     res.json({
-//         setupIntent: setupIntent.client_secret,
-//         ephemeralKey: ephemeralKey.secret,
-//         customer: customer.id,
-//         publishableKey: 'pk_test_51QCQ22FtcuUTeLbKDvnvy8gGAuaeGDOTqjCgMjp8FGXvzdtRNHroyDrG6oZwPackuIHdiGSg0FTXwFcDweegTUey00VmF3aOgV'
-//     })
-// });
