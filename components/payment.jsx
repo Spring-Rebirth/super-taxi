92b1108a4635285427a223ebaf46148e101fb0c1
Spring-Rebirth <cjunwei6249@gmail.com>
2024-10-22 19:25:42 +0800

    补充Stripe初步配置

--- components/payment.jsx
+++ components/payment.jsx
@@ -1,10 +1,11 @@
 import { View, Text, Alert } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import CustomButton from './CustomButton'
 import { PaymentSheetError, useStripe } from '@stripe/stripe-react-native'
 
 export default function Payment() {
     const { initPaymentSheet, presentPaymentSheet } = useStripe();
+    const [success, setSuccess] = useState(false);
 
     const initializePaymentSheet = async () => {
         const {
@@ -18,31 +19,26 @@ export default function Payment() {
 
     }
 
+    const openPaymentSheet = async () => {
+        await initializePaymentSheet();
 
-
-    const openPaymentSheet = () => {
         const { error } = await presentPaymentSheet();
 
         if (error) {
             Alert.alert(`Error code: ${error.code}`, error.message);
-            if (error.code === PaymentSheetError.Canceled) {
-
-            } else {
-
-            }
-
         } else {
             Alert.alert('Success', 'Your payment method is successfully set up for future payments!');
+            setSuccess(true);
         }
     };
-}
 
-return (
-    <View className='my-10'>
-        <CustomButton
-            onPress={openPaymentSheet}
-            title={'Confirm Ride'}
-        />
-    </View>
-)
+
+    return (
+        <View className='my-10'>
+            <CustomButton
+                onPress={openPaymentSheet}
+                title={'Confirm Ride'}
+            />
+        </View>
+    )
 }
