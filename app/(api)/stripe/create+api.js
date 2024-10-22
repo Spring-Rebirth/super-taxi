6e71a7430790cc974d70d71be1ffea000c76cded
Spring-Rebirth <cjunwei6249@gmail.com>
2024-10-22 21:10:35 +0800

    修改默认内容3

--- app/(api)/stripe/create+api.js
+++ app/(api)/stripe/create+api.js
@@ -11,29 +11,44 @@ export async function POST(request) {
                 error: 'Please enter a valid email address',
                 status: 400
             })
-        )
+        );
+    }
+
+    let customer;
+
+    const existingCustomer = await stripe.customers.list({ email });
+
+    if (existingCustomer.data.length > 0) {
+        customer = existingCustomer.data[0];
+    } else {
+        const newCustomer = await stripe.customers.create({
+            name,
+            email
+        });
+
+        customer = newCustomer;
     }
 }
 
-app.post('/payment-sheet', async (req, res) => {
-    // Use an existing Customer ID if this is a returning customer.
-    const customer = await stripe.customers.create();
-    const ephemeralKey = await stripe.ephemeralKeys.create(
-        { customer: customer.id },
-        { apiVersion: '2024-09-30.acacia' }
-    );
-    const setupIntent = await stripe.setupIntents.create({
-        customer: customer.id,
-        // In the latest version of the API, specifying the `automatic_payment_methods` parameter
-        // is optional because Stripe enables its functionality by default.
-        automatic_payment_methods: {
-            enabled: true,
-        },
-    });
-    res.json({
-        setupIntent: setupIntent.client_secret,
-        ephemeralKey: ephemeralKey.secret,
-        customer: customer.id,
-        publishableKey: 'pk_test_51QCQ22FtcuUTeLbKDvnvy8gGAuaeGDOTqjCgMjp8FGXvzdtRNHroyDrG6oZwPackuIHdiGSg0FTXwFcDweegTUey00VmF3aOgV'
-    })
-});
+// app.post('/payment-sheet', async (req, res) => {
+//     // Use an existing Customer ID if this is a returning customer.
+//     const customer = await stripe.customers.create();
+//     const ephemeralKey = await stripe.ephemeralKeys.create(
+//         { customer: customer.id },
+//         { apiVersion: '2024-09-30.acacia' }
+//     );
+//     const setupIntent = await stripe.setupIntents.create({
+//         customer: customer.id,
+//         // In the latest version of the API, specifying the `automatic_payment_methods` parameter
+//         // is optional because Stripe enables its functionality by default.
+//         automatic_payment_methods: {
+//             enabled: true,
+//         },
+//     });
+//     res.json({
+//         setupIntent: setupIntent.client_secret,
+//         ephemeralKey: ephemeralKey.secret,
+//         customer: customer.id,
+//         publishableKey: 'pk_test_51QCQ22FtcuUTeLbKDvnvy8gGAuaeGDOTqjCgMjp8FGXvzdtRNHroyDrG6oZwPackuIHdiGSg0FTXwFcDweegTUey00VmF3aOgV'
+//     })
+// });
