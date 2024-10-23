5fddfe8a02b4b5aa549af8682d015824589a2314
Spring-Rebirth <cjunwei6249@gmail.com>
2024-10-23 23:05:38 +0800

    修改路由导航路径2

--- components/payment.jsx
+++ components/payment.jsx
@@ -82,7 +82,7 @@ const Payment = () => {
                         containerStyle={"mt-5"}
                         onPress={() => {
                             setSuccess(false);
-                            router.push("/(tabs)/home");
+                            router.replace("/(tabs)/home");
                         }}
                     />
                 </View>
